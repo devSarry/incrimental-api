6e7cecf0bc7a8907b3e64ad949baa1e4bc54bbf3
dopyoman <jsarry@gmail.com>
2015-10-10 16:06:59 +0300

    separated the error response method. Now allowing for a build up of the error message

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -15,6 +15,9 @@ use Illuminate\Support\Facades\Response;
 class ApiController extends Controller {
 
     /*Our Api Controller needs some way to set the status code.*/
+    /**
+     * @var int
+     */
     protected $statusCode = 200;
 
     /**
@@ -27,17 +30,52 @@ class ApiController extends Controller {
 
     /**
      * @param int $statusCode
+     * @return $this
      */
     public function setStatusCode($statusCode)
     {
         $this->statusCode = $statusCode;
+
+        return $this;
     }
 
+    /**
+     * @param string $message
+     * @return mixed
+     */
     public function respondNotFound($message = 'Not Found')
     {
-        return Response::json([
-            'error' => $message,
-            'status_code' => $this->getStatusCode()
+        return $this->setStatusCode(404)->respondWithError($message);
+    }
+
+    /*We want to make this a bit more flexible so that we wouldnt have to duplicate the code above for
+    every type of error. What we really want to do is somethign like
+             $this->setStatusCode(404)->respondWithError('not found')*/
+
+
+    /*first we need to extract the Response part*/
+    /**
+     * @param $data
+     * @param array $header
+     * @return mixed
+     */
+    public function respond($data, $header = [])
+    {
+        return Response::json($data, $this->getStatusCode(), $header);
+    }
+
+    /*Now whenever we start building our error message it will be stored in this errors array*/
+    /**
+     * @param $message
+     * @return mixed
+     */
+    public function respondWithError($message)
+    {
+        return $this->respond([
+            'error' => [
+                'message'     => $message,
+                'status_code' => $this->getStatusCode()
+            ]
         ]);
     }
 
