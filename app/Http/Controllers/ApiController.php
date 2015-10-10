c1725a0aef040c41e5c3f743c5eaf310bd1fc16b
dopyoman <jsarry@gmail.com>
2015-10-10 15:44:31 +0300

    API Controller class object needs a status code

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -8,9 +8,28 @@
 
 namespace App\Http\Controllers;
 
-
+/*This class extends our base controller which is being extended by our LessonsController
+        LessonsController->ApiController->Controller->BaseController*/
 class ApiController extends Controller {
 
-    
+    /*Our Api Controller needs some way to set the status code.*/
+    protected $statusCode = 200;
+
+    /**
+     * @return int
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * @param int $statusCode
+     */
+    public function setStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
+    }
+
 
 }
